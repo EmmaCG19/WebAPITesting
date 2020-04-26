745d444f40f04cc40bb1730652fde9b031c88b09
Emmanuel Fernández <emmanuelfernandez600@gmail.com>
2020-04-26 02:26:17 -0300

    Added some basic CRUD operations.

--- SchoolAPI/Controllers/StudentsController.cs
+++ SchoolAPI/Controllers/StudentsController.cs
@@ -43,6 +43,40 @@ namespace SchoolAPI.Controllers
             return Ok(students);
         }
 
+        [HttpGet]
+        [Route("api/students/{name:alpha}")]
+        public IHttpActionResult GetStudentsByName(string name, bool addressIncluded = false)
+        {
+            IList<StudentVM> studentsByName = null;
+
+            using (var context = new SchoolDBEntities())
+            {
+                studentsByName = context.Students
+                    .Include("StudentAddress")
+                    .Where(s => s.StudentName.ToLower().Equals(name.ToLower()))
+                    .Select(s => new StudentVM()
+                    {
+                        Id = s.StudentID,
+                        FullName = s.StudentName,
+                        Address = s.StudentAddress == null || addressIncluded == false ? null :
+                        new StudentAdressVM()
+                        {
+                            StudentId = s.StudentID,
+                            City = s.StudentAddress.City,
+                            State = s.StudentAddress.State,
+                            Address1 = s.StudentAddress.Address1,
+                            Address2 = s.StudentAddress.Address2
+
+                        }
+                    })
+                    .ToList<StudentVM>();
+            }
+
+            if (studentsByName.Count == 0)
+                return NotFound();
+
+            return Ok(studentsByName);
+        }
 
         public IHttpActionResult GetStudentById(int id)
         {
@@ -75,13 +109,66 @@ namespace SchoolAPI.Controllers
             {
                 context.Students.Add(new Student()
                 {
-                    StudentName = student.FullName
+                    StudentID = student.Id,
+                    StudentName = student.FullName,
+                    StandardId = 3
                 });
 
                 context.SaveChanges();
             }
 
-            return Ok(new StringContent(String.Format("El alumno se generó con el id {0}", student.Id)));
+
+            //Devolver el estudiante con el id generado
+            return Ok();
+
+        }
+
+        public IHttpActionResult PutStudent(StudentVM student)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Los datos ingresados son inválidos.");
+
+            using (var context = new SchoolDBEntities())
+            {
+                var estExistente = context.Students
+                                .Where(s => s.StudentID == student.Id)
+                                .FirstOrDefault<Student>();
+
+                if (estExistente != null)
+                {
+                    estExistente.StudentName = student.FullName;
+                    context.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+            }
+
+            return Ok();
+        }
+
+
+        public IHttpActionResult DeleteStudent(int id)
+        {
+            if (id < 0)
+                return BadRequest("El id es inválido.");
+
+            using (var context = new SchoolDBEntities())
+            {
+                var student = context.Students.Where(s => s.StudentID == id).FirstOrDefault<Student>();
+
+                if (student != null)
+                {
+                    context.Students.Remove(student);
+                    context.SaveChanges();
+                }
+                else
+                    return NotFound();
+
+                return Ok();
+            }
 
         }
     }
