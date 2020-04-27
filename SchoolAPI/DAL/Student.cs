e04b7119191ebd8ecb2428ed3ee4489463400fa6
Emmanuel Fernández <emmanuelfernandez600@gmail.com>
2020-04-27 01:39:38 -0300

    Creating a database with Entity Framework 6 - Code First.

---
+++ SchoolAPI/DAL/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace SchoolAPI.DAL
+{
+    public class Student
+    {
+        [Key, Required]
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public DateTime BornDate { get; set; }
+
+        public virtual StudentAddress Address { get; set; }
+        public virtual Standard Standard { get; set; }
+
+    }
+}
